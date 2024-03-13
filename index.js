57ec4f6c7182c81b3771deee26990a21da118f4e
NISVAN--MOHAMED <niswan19@gmail.com>
2024-03-13 19:00:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var inputvalue = document.querySelector('#cityinput')
+var btn = document.querySelector('#add')
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+apik="c4299c87d45886f18553d3f3ae5ab6d4"
+function convertion(val)
+{
+    return (val - 273).toFixed(3)
+}
+
+btn.addEventListener('click',function()
+{
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+apik)
+    .then(res => res.json())
+
+    .then(data =>
+    {
+       var nameval = data['name']
+       var descrip = data['weather']['0']['description']
+       var tempature = data['main']['temp']
+       var wndspeed = data['wind']['speed']
+
+       city.innerHTML=`Weather of <span>${nameval}<span>`
+       temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
+       description.innerHTML = `Sky Conditions: <span>${descrip}<span>` 
+       wind.innerHTML =`Wind Speed: <span>${wndspeed} km/h<span>`
+    })
+
+    .catch(err => alert('city not found'))
+})
